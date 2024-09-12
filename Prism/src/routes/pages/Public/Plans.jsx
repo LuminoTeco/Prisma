39367f93dd50ca234dd2c7a69960b85df803b315
Erick Gonçalves <erick.silva312@etec.sp.gov.br>
2024-09-12 01:14:26 -0300

    botão

--- Prism/src/routes/pages/Public/Plans.jsx
+++ Prism/src/routes/pages/Public/Plans.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
-
+import './plans.css'
 const Plans = () => {
   return (
     <div>
   
       <nav>
-        <img src="/src/routes/pages/Public/imagens/Logo.svg" alt="" />  
+        <img src="/src/routes/pages/Public/imagens/Logo.svg" alt="logo prisma" />  
       </nav>
   
       <div className='container'>
+
         <div className='esquerda'>
-        <h1>Plano</h1>
-        <h1>ESCOLA</h1>
-        <p>Perfeito para as intituições de ensino menores, que querem melhorar a experiencia dos seus alunos e trabalhar a colaboração e conexão entre eles! </p>
+         <h1>Plano</h1>
+         <h1>ESCOLA</h1>
+         <p>Perfeito para as intituições de ensino menores, que querem melhorar a experiencia dos seus alunos e trabalhar a colaboração e conexão entre eles! </p>
         </div>
+      
+        <div className='direita'>
+          <div className='card'>
+           <img src="/src/routes/pages/Public/imagens/Image.png" alt="card com informações do plano escola" />
+          </div>
+        </div>
+        
+        <button>Assinar</button> 
+          <br/> 
+          <img src="/src/routes/pages/Public/imagens/rainbow.png" alt="arco-íris" />
+          
       </div>
-  
-      <div className='direita'></div>
-        <div>
-          <img src="/src/routes/pages/Public/imagens/Image.png" alt="" />
-        </div>
-      <button>Assinar</button>  
-      <img src="/src/routes/pages/Public/imagens/rainbow.png" alt="" />
+
 
     </div>
   )
