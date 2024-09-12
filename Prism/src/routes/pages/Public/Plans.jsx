f119ea7dde46257b8c72905335b828736b14d624
Erick Gonçalves <erick.silva312@etec.sp.gov.br>
2024-09-12 00:11:27 -0300

    plano escola
    iniciando a tela designada para o plano escola

--- Prism/src/routes/pages/Public/Plans.jsx
+++ Prism/src/routes/pages/Public/Plans.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const Plans = () => {
   return (
     <div>
-      <h1>Planos</h1>
+  
+      <nav>
+        <img src="/src/routes/pages/Public/imagens/Logo.svg" alt="" />  
+      </nav>
+  
+      <div className='container'>
+        <div className='esquerda'>
+        <h1>Plano</h1>
+        <h1>ESCOLA</h1>
+        <p>Perfeito para as intituições de ensino menores, que querem melhorar a experiencia dos seus alunos e trabalhar a colaboração e conexão entre eles! </p>
+        </div>
+      </div>
+  
+      <div className='direita'></div>
+        <div>
+          <img src="/src/routes/pages/Public/imagens/Image.png" alt="" />
+        </div>
+      <button>Assinar</button>  
+      <img src="/src/routes/pages/Public/imagens/rainbow.png" alt="" />
+
     </div>
   )
 }
