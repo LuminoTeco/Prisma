b568b55c98c0a7e352a8ef123c583ff456046669
Erick Gonçalves <erick.silva312@etec.sp.gov.br>
2024-09-16 00:09:45 -0300

    ajustando css

--- Prism/src/routes/pages/Public/Plans.jsx
+++ Prism/src/routes/pages/Public/Plans.jsx
@@ -4,28 +4,23 @@ const Plans = () => {
   return (
     <div>
   
-      <nav>
-        <img src="/src/routes/pages/Public/imagens/Logo.svg" alt="logo prisma" />  
-      </nav>
-  
       <div className='container'>
 
         <div className='esquerda'>
-         <h1>Plano</h1>
-         <h1>ESCOLA</h1>
-         <p>Perfeito para as intituições de ensino menores, que querem melhorar a experiencia dos seus alunos e trabalhar a colaboração e conexão entre eles! </p>
+          <h1>Plano</h1>
+          <h1>ESCOLA</h1>
+          <p>Perfeito para as intituições de ensino menores, que querem melhorar a experiencia dos seus alunos e trabalhar a colaboração e conexão entre eles!</p>
+          <br /><br /><br /><br /><br />
+          <button>Assinar</button>
+          <br />
+          <img src="/src/routes/pages/Public/imagens/rainbow.png" alt="arco-íris"/>
         </div>
-      
+
         <div className='direita'>
-          <div className='card'>
-           <img src="/src/routes/pages/Public/imagens/Image.png" alt="card com informações do plano escola" />
-          </div>
+          <img src="/src/routes/pages/Public/imagens/Image.png" alt="card do plano escola" className='card-imagem'/>
         </div>
+
         
-        <button>Assinar</button> 
-          <br/> 
-          <img src="/src/routes/pages/Public/imagens/rainbow.png" alt="arco-íris" />
-          
       </div>
 
 
